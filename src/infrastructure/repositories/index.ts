80750f07c93f5e09fbefc937f17942e98202baef
randyhytech <randy.martinez@hytechhome.com>
2025-07-25 19:14:54 -0400

    chore: :fire: Remove unnecessary comments

--- src/infrastructure/repositories/index.ts
+++ src/infrastructure/repositories/index.ts
@@ -1,19 +1,10 @@
 import { AxiosClient } from "../api/AxiosClient";
 import { TodoRepositoryImpl } from "../repositories/TodoRepositoryImpl";
 
-/**==============
- * HTTP CLIENT
-=================*/
 const httpClient = new AxiosClient();
 
-/**=================================
- * IMPLEMENTATION OF REPOSITORIES
-====================================*/
 const todoRepository = new TodoRepositoryImpl(httpClient);
 
-/**=================================================
- * EXPORTING REPOSITORIES FOR USE IN PINIA STORES
-====================================================*/
 export {
   todoRepository
 };
