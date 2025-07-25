84af4732f1ebe77312e99fa19a9b3b80c86a6253
randyhytech <randy.martinez@hytechhome.com>
2025-07-25 16:22:35 -0400

    feat: :sparkles: Fix import issues

--- src/presentation/router/index.ts
+++ src/presentation/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../modules/home/HomeView.vue';
+import HomeView from '../modules/home/pages/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
