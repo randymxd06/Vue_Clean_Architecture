dfd7e02ce6e52b54ee93fc77807c085512b1297b
randymxd06 <randym0624@gmail.com>
2025-08-02 17:38:26 -0400

    test: :test_tube: TodoRepository with type errors

--- src/domain/repositories/TodoRepository.ts
+++ src/domain/repositories/TodoRepository.ts
@@ -1,9 +1,9 @@
 import type { Todo } from "../entities/Todo";
 
 export interface TodoRepository {
-	getTodos(): Promise<Todo[]>;
-	getTodoById(id: string): Promise<Todo>;
-	createTodo(todo: Omit<Todo, "id">): Promise<Todo>;
+	getTodos(): Promise<any[]>;
+	getTodoById(id: any): Promise<Todo>;
+	createTodo(todo: Omit<number, "id">): Promise<number>;
 	updateTodo(todo: Todo): Promise<Todo>;
 	deleteTodo(id: string): Promise<Todo>;
 }
