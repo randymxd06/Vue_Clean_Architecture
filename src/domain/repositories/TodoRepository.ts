00ce3056a18edf5dfbe540bdafe23880c2c65cd8
randymxd06 <randym0624@gmail.com>
2025-08-02 17:39:52 -0400

    fix: :bug: fix TodoRepository types

--- src/domain/repositories/TodoRepository.ts
+++ src/domain/repositories/TodoRepository.ts
@@ -1,9 +1,9 @@
 import type { Todo } from "../entities/Todo";
 
 export interface TodoRepository {
-	getTodos(): Promise<any[]>;
-	getTodoById(id: any): Promise<Todo>;
-	createTodo(todo: Omit<number, "id">): Promise<number>;
+	getTodos(): Promise<Todo[]>;
+	getTodoById(id: string): Promise<Todo>;
+	createTodo(todo: Omit<Todo, "id">): Promise<Todo>;
 	updateTodo(todo: Todo): Promise<Todo>;
 	deleteTodo(id: string): Promise<Todo>;
 }
