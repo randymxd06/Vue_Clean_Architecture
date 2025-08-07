7bb3dab25c0b106cd97181f2d94dee6dc0b263c8
randyhytech <randy.martinez@hytechhome.com>
2025-08-07 15:00:33 -0400

    feat: :sparkles: I added transitions between Login and Register.

--- src/presentation/components/layouts/AuthLayout.vue
+++ src/presentation/components/layouts/AuthLayout.vue
@@ -1,22 +1,3 @@
-<template>
-    <main class="min-h-screen flex flex-col md:flex-row bg-blue-600" :class="{'md:flex-row-reverse': reverse}">
-        <section class="w-full md:w-1/2 text-white flex items-center justify-center flex-col px-6 py-16">
-            <div class="text-center">
-                <h1 class="text-2xl md:text-4xl font-bold mb-2">Welcome to YourApp</h1>
-                <p class="text-sm md:text-lg">We made it for you</p>
-            </div>
-        </section>
-
-        <section
-            class="w-full md:w-1/2 flex items-center justify-center p-6 bg-white shadow-md rounded-t-[1.5rem] md:rounded-l-[1.5rem] md:rounded-tr-none flex-grow h-full md:h-auto"
-            :class="{'md:rounded-l-none! md:rounded-r-[1.5rem]!': reverse}">
-            <div class="w-full max-w-md">
-                <router-view />
-            </div>
-        </section>
-    </main>
-</template>
-
 <script>
 export default {
     props: {
@@ -27,3 +8,36 @@ export default {
     }
 }
 </script>
+
+<template>
+    <main class="min-h-screen flex flex-col md:flex-row bg-blue-600" :class="{ 'md:flex-row-reverse': reverse }">
+        <section class="w-full md:w-1/2 text-white flex items-center justify-center flex-col px-6 py-16">
+            <div class="text-center">
+                <h1 class="text-2xl md:text-4xl font-bold mb-2">Welcome to YourApp</h1>
+                <p class="text-sm md:text-lg">We made it for you</p>
+            </div>
+        </section>
+
+        <section
+            class="w-full md:w-1/2 flex items-center justify-center p-6 bg-white shadow-md rounded-t-[1.5rem] md:rounded-l-[1.5rem] md:rounded-tr-none flex-grow h-full md:h-auto"
+            :class="{ 'md:rounded-l-none! md:rounded-r-[1.5rem]!': reverse }">
+            <div class="w-full max-w-md">
+                <transition name="fade" mode="out-in">
+                    <router-view />
+                </transition>
+            </div>
+        </section>
+    </main>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 1s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
