c1999b3d5b2cdc2bd90f51ae2e669957f12b6b9e
randymxd06 <randym0624@gmail.com>
2025-08-22 00:50:24 -0400

    feat: :sparkles: Add a notification icon

--- src/presentation/components/layouts/AppLayout.vue
+++ src/presentation/components/layouts/AppLayout.vue
@@ -1,11 +1,16 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+const sidebarVisible = ref(true);
+</script>
+
 <template>
 
-    <div class="h-screen flex bg-gray-100">
+    <main class="h-screen flex bg-gray-100">
 
         <!--==========
             SIDEBAR
         ==============-->
-        <aside class="w-64 flex flex-col bg-gray-100">
+        <aside v-show="sidebarVisible" class="w-64 flex flex-col bg-gray-100 transition-all duration-300">
 
             <!--==================
                 LOGO / APP NAME
@@ -179,12 +184,12 @@
         <!--====================
             MAIN CONTENT AREA
         ========================-->
-        <main class="flex-1 flex flex-col overflow-hidden pr-2 py-2">
+        <section class="flex-1 flex flex-col overflow-hidden pr-2 pt-2" :class="{ 'pl-2': !sidebarVisible }">
 
             <!--===========================
                 CONTENT CARD WITH TOPBAR
             ===============================-->
-            <section class="bg-white rounded-xl shadow-sm flex-1 flex flex-col overflow-hidden">
+            <article class="bg-white rounded-t-xl shadow-sm flex-1 flex flex-col overflow-hidden">
 
                 <!--==========
                     TOP BAR
@@ -196,7 +201,7 @@
                         <!--=============================
                             HIDE / SHOW SIDEBAR BUTTON
                         =================================-->
-                        <button type="button" class="cursor-pointer">
+                        <button type="button" class="cursor-pointer" @click="sidebarVisible = !sidebarVisible">
                             <svg width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
                                 fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" class="feather feather-sidebar">
@@ -225,8 +230,8 @@
                     <section class="flex items-center space-x-4">
 
                         <!--=========
-                                SEARCH
-                            =============-->
+                            SEARCH
+                        =============-->
                         <article class="relative">
                             <input type="text" placeholder="Buscar..."
                                 class="w-64 pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent">
@@ -238,9 +243,9 @@
                         </article>
 
                         <!--===============
-                                THEME TOGGLE
-                            ===================-->
-                        <button class="p-2 rounded-lg hover:bg-gray-100 transition-colors font-semibold">
+                            THEME TOGGLE
+                        ===================-->
+                        <button class="p-2 rounded-lg hover:bg-gray-100 transition-colors font-semibold cursor-pointer">
                             <svg class="w-5 h-5 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
@@ -248,19 +253,18 @@
                         </button>
 
                         <!--================
-                                NOTIFICATIONS
-                            ====================-->
-                        <button class="p-2 rounded-lg hover:bg-gray-100 relative transition-colors font-semibold">
-                            <svg class="w-5 h-5 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M15 17h5l-5-5V9a9 9 0 10-6 0v8l-5 5h5m0 0a3 3 0 01-6 0" />
+                            NOTIFICATIONS
+                        ====================-->
+                        <button class="p-2 rounded-lg hover:bg-gray-100 relative transition-colors font-semibold cursor-pointer">
+                            <svg class="w-5 h-5 text-gray-600" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path fill="currentColor" d="M224 0c-8.8 0-16 7.2-16 16l0 16.8C127.1 40.8 64 109 64 192l0 10.3c0 45.1-15.4 88.9-43.5 124.1L10.4 339C3.7 347.4 0 357.9 0 368.6 0 394.8 21.2 416 47.4 416l353.2 0c26.2 0 47.4-21.2 47.4-47.4 0-10.8-3.7-21.2-10.4-29.6l-10.1-12.6c-28.2-35.2-43.5-79-43.5-124.1l0-10.3c0-83-63.1-151.2-144-159.2L240 16c0-8.8-7.2-16-16-16zm0 64c70.7 0 128 57.3 128 128l0 10.3c0 52.4 17.8 103.2 50.6 144.1L412.6 359c2.2 2.7 3.4 6.1 3.4 9.6 0 8.5-6.9 15.4-15.4 15.4L47.4 384c-8.5 0-15.4-6.9-15.4-15.4 0-3.5 1.2-6.9 3.4-9.6l10.1-12.6C78.2 305.5 96 254.7 96 202.3L96 192c0-70.7 57.3-128 128-128zM156.1 464c9.9 28 36.6 48 67.9 48s58-20 67.9-48L256 464c-7.3 9.7-18.9 16-32 16s-24.7-6.3-32-16l-35.9 0z"/>
                             </svg>
                             <span class="absolute top-1 right-1 w-2 h-2 bg-red-500 rounded-full"></span>
                         </button>
 
                         <!--=============
-                                ADD BUTTON
-                            =================-->
+                            ADD BUTTON
+                        =================-->
                         <button
                             class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 flex items-center space-x-2 transition-colors">
                             <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -286,10 +290,10 @@
 
                 </main>
 
-            </section>
+            </article>
 
-        </main>
+        </section>
 
-    </div>
+    </main>
 
 </template>
